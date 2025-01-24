d42284c5001a3c4942ba4c97bce77407de33a5dc
João Marcelo <156209233+ojotaeme@users.noreply.github.com>
2025-01-23 23:55:58 -0300

    map

---
+++ map/map/src/application/Program.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Map<String, String> cookies = new TreeMap<>();
+		
+		cookies.put("username", "Maria");
+		cookies.put("email", "maria@email.com");
+		cookies.put("phone", "987654321");
+		
+		cookies.remove("email");
+		cookies.put("phone", "123456789");
+		
+		System.out.println("Contains 'phone' key: " + cookies.containsKey("phone"));
+		System.out.println("Phone number: " + cookies.get("phone"));
+		System.out.println("Email: " + cookies.get("email"));
+		System.out.println("Size: " + cookies.size());
+		
+		System.out.println("All cookies");
+		
+		for (String key : cookies.keySet()) {
+			System.out.println(key + ": " + cookies.get(key));
+		}
+		
+	}
+
+}
